7b84ab9c79f85e3aa7cf872c7757c62edf0b5cad
YagoYJ <yagojuanyjskt@gmail.com>
2023-10-28 20:05:49 -0300

    feat: Initial configurations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Yago Juan Dev',
-  description: "I'm a Full Stack developer",
+  description: "I'm a Full Stack developer"
 }
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: {
   children: React.ReactNode
 }) {
